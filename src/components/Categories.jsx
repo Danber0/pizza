5f71d8d2511776da0f7cd7de879edb4a35e57b49
Danber <bergauzen.danil@mail.ru>
2021-10-06 18:18:33 +0300

    Add some components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+// rfce
+import React from 'react';
+
+function Categories({ items, onClickItem }) {
+  const [activeitem, setActiveItem] = React.useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        <li className={activeitem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeitem === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}>
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
